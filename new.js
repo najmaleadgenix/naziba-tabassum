b0201f93e8e4355f30a01e8c6c05a26a59d6ab73
Najma Nahar <154003670+najmaleadgenix@users.noreply.github.com>
2026-06-13 09:25:45 +0600

    Add files via upload

---
+++ new.js
@@ -0,0 +1,151 @@
+// var test='meherima';
+// var num = 9;
+// var mmn=false;
+
+// var obj = {
+//     name : "naziba"
+// };
+// console.log(typeof test);
+// console.log(typeof num);
+// console.log(typeof mmn);
+// console.log(typeof obj);
+
+// var num1="12.4";
+// var num2= 2;
+// console.log(parseFloat(num1) + num2);
+
+// var stat = "gorom";
+// if(stat == "rain")
+// {
+//     console.log("baire jawa jabe na its raining");
+// }
+// else if(stat == "gorom")
+// {
+//     console.log("its hot outside so you can't go");
+// }
+// else 
+// {
+//     console.log("you can go");
+// }
+
+// var obj={
+//     eye:2,
+//     hand:2,
+//     person:{
+//         name: "Naziba",
+//         age:20
+//     }
+// }
+// console.log(obj.person.name);
+
+// var friends=[2,3,"naziba", {obj:"person"}, ["one", "two"]];
+// friends.push(999);
+// friends.pop()
+// friends.unshift("Jjjik");
+// friends.shift()
+// console.log(friends);
+// for(var i=0; i<friends.length; i++)
+// {
+//     console.log(friends[i]);
+// }
+
+// function sum(num1,num2){
+//     var result = num1+num2
+//     console.log(result)
+// }
+// sum("naz", "iba")
+
+// var num=4
+// var num= 3
+// console.log(num)
+
+// function sum()
+// {
+//     if(true)
+//     {
+//         const h="naz"
+//     }
+//     if(true)
+//     {
+//         console.log(h)
+//     }
+// }
+// sum()
+
+// const num = 5
+// const num = 3
+// console.log(num)
+
+// const num =5
+// num = 10
+// console.log(num)
+
+// let num =10
+// num =4
+// console.log(num)
+
+// const countryName="bangladesh"
+// const country = `this is my county ${countryName}`
+// console.log(country)
+
+// const num = [1,2,3,4,5]
+// const num1 = [6,7,8,9,10]
+// console.log([...num1,...num])
+
+// const person = {
+//     name : "Naziba",
+//     age: 20,
+//     friends: ['rafa','Arnob','karshi']
+// }
+// // const age = person.age
+// // console.log(age)
+// const {friends,age} = person
+// console.log(friends)
+
+// const person = ['naziba', 20, 'kmdk ldfoa lsddf']
+// const [a,b,c] = person
+// console.log(c)
+
+// function sum(num1, num2){
+//     return num1+num2
+// }
+// const result = sum(20,30)
+// console.log(result)
+
+// const sum = (num1,num2) =>{
+//     return num1+num2
+// }
+// const output = sum(12,2)
+// console.log(output)
+
+// const arr = [
+//     {id:1, name:"Iphone", description:"this is Iphone", price:800, color:"golden"},
+//     {id:2, name:"xiaomi", description:"this is xiaomi", price:500, color:"black"},
+//     {id:3, name:"Iphone", description:"this is Iphone", price:1000, color:"gray"},
+//     {id:4, name:"xiaomi", description:"this is xiaomi", price:500, color:"black"},
+// ]
+// for(let i=0; i<arr.length; i++){
+//     const element = arr[i];
+//     if(element.id==3){
+//         console.log(element)
+//     }
+// }
+
+// const result = arr.find(pd => pd.id==3)
+// console.log(result)
+
+// const result= arr.filter(pd=> pd.color=='black')
+// console.log(result)
+
+// const result = arr.map(sd=> sd.id*2)
+// console.log(result)
+
+// const result = arr.forEach(pd=>{
+//     console.log(pd.id)
+// })
+
+// console.log(result)
+
+
+
+
